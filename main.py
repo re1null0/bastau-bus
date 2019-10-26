1bd353ae0766acd5b3d550e22361b68858859239
re1null0 <34448123+re1null0@users.noreply.github.com>
2019-10-26 19:12:50 +0600

    deleted favicon

--- main.py
+++ main.py
@@ -20,11 +20,7 @@ def stream():
 def feedback():
     return render_template("report.html")
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-   
-""""lalalallalala"""
