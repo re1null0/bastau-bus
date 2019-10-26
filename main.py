df81540e9e026f06afea5badd86273fde98f569f
re1null0 <34448123+re1null0@users.noreply.github.com>
2019-10-26 19:49:36 +0600

    update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from flask import send_from_directory
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
 def admin():
